5454ff49b2942aae45f967ece8ce9d924931fd99
Prattush Vyas <prattushvyas@gmail.com>
2023-06-08 01:53:20 +0530

    up

--- script.js
+++ script.js
@@ -3,19 +3,18 @@ const bar = document.getElementById('bar');
 const nav = document.getElementById('navbar');
 const close = document.getElementById('close');
 
-if(bar){
-    bar.addEventListener('click',() =>
-    {
-        nav.classList.add('active');
-       
-    }
-    )
+if (bar) {
+  bar.addEventListener('click', () => {
+    nav.classList.add('active');
+
+  }
+  )
 }
 
-if(close){
-    close.addEventListener('click' ,() => {
-        nav.classList.remove('active')
-    })
+if (close) {
+  close.addEventListener('click', () => {
+    nav.classList.remove('active')
+  })
 }
 
 
@@ -26,44 +25,75 @@ let countEl = document.getElementById("count-el")
 let count = 0
 
 function increment() {
-    count += 1
-    countEl.textContent = count
+  count += 1
+  countEl.textContent = count
 }
 
 //coina
-    // Check if the paragraph value is stored in the local storage
-    if (!localStorage.getItem('coins')) {
-        // If not, set the initial value to 1000
-        localStorage.setItem('coins', 1000);
-      }
-      
-      // Get the paragraph element and the subtract button element
-      const coinsParagraph = document.getElementById('coins');
-      const subtractButton = document.getElementById('subtractButton');
-      
-      // Update the paragraph text with the stored value from local storage
-      coinsParagraph.textContent = localStorage.getItem('coins');
-      
-      // Add an event listener to the subtract button
-      subtractButton.addEventListener('click', function() {
-        // Get the current value from the paragraph
-        let currentValue = parseInt(coinsParagraph.textContent);
-      
-        // Subtract 1 from the current value
-        currentValue--;
-      
-        // Update the paragraph text with the new value
-        coinsParagraph.textContent = currentValue;
-      
-        // Store the new value in the local storage
-        localStorage.setItem('coins', currentValue);
-      });
+// Check if the paragraph value is stored in the local storage
+if (!localStorage.getItem('coins')) {
+  // If not, set the initial value to 1000
+  localStorage.setItem('coins', 1000);
+}
 
-      //Modal For LogIn and SignUp
-       function visible() {
-        document.getElementById('modalContainer').style.display = 'block';
-      };
-      
-      function notVisible()  {
-        document.getElementById('modalContainer').style.display = 'none';
-      };
+// Get the paragraph element and the subtract button element
+const coinsParagraph = document.getElementById('coins');
+const subtractButton = document.getElementById('subtractButton');
+
+// Update the paragraph text with the stored value from local storage
+coinsParagraph.textContent = localStorage.getItem('coins');
+
+// Add an event listener to the subtract button
+subtractButton.addEventListener('click', function () {
+  // Get the current value from the paragraph
+  let currentValue = parseInt(coinsParagraph.textContent);
+
+  // Subtract 1 from the current value
+  currentValue--;
+
+  // Update the paragraph text with the new value
+  coinsParagraph.textContent = currentValue;
+
+  // Store the new value in the local storage
+  localStorage.setItem('coins', currentValue);
+});
+
+//Modal For LogIn and SignUp
+function visible() {
+  document.getElementById('modalContainer').style.display = 'block';
+};
+
+function notVisible() {
+  document.getElementById('modalContainer').style.display = 'none';
+};
+// Login Modal
+function login() {
+  var email = document.getElementById("email").value;
+  var password = document.getElementById("password").value;
+  // Check if the email and password match
+  if (email === "roshan@gmail.com" && password === "123") {
+    // Change the HTML element to an anchor link
+    var messageElement = document.getElementById("login");
+    messageElement.innerHTML = "<a href='account-settings/index.html'>Account</a>";
+    localStorage.setItem("isLoggedIn", true);
+    notVisible();
+  } else {
+    // // Show an error message
+    // var errorElement = document.getElementById("error");
+    // errorElement.innerHTML = "Invalid email or password";
+    alert("invalid Username or Password!");
+  }
+}
+
+function checkLoginStatus() {
+  var isLoggedIn = localStorage.getItem("isLoggedIn");
+  var messageElement = document.getElementById("login");
+
+  if (isLoggedIn === "true") {
+    messageElement.innerHTML = "<a href='account-settings/index.html'>Account</a>";
+    notVisible();
+  } else {
+    messageElement.innerHTML = `<li id="login"><a href='#' onclick= "visible();" >Login</a></li>`;
+  }
+}
+checkLoginStatus();
